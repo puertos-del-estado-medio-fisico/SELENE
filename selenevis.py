6bc8af13ceb94fbc7b5bbf584000189931c628b2
bpergom <56068686+bpergom@users.noreply.github.com>
2019-10-09 17:21:21 +0200

    Update selenevis.py
    To deal with None data in the time series, as selene.py

--- selenevis.py
+++ selenevis.py
@@ -2,14 +2,23 @@
 selene
 Created on 9 jun. 2018
 @author: AMG
+@modified: bpergom, october 2019
 '''
 import sys
+import logging
 import configparser
 import utils.visualize as visualize
 import utils.iofilehandler as iofilehandler
 import json
 import configuration.constants as c
+#log
+logger = logging.getLogger('selene')
+fh = logging.FileHandler(c.logfile)
+fh.setFormatter(logging.Formatter('%(asctime)s %(station)s %(levelname)s %(message)s'))
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
 #config
+logger.info('selenevis tool >> started!')
 config = configparser.ConfigParser()
 config.read(c.configini)
 option = sys.argv[1]
@@ -18,14 +27,14 @@ with open(c.stationsfile) as f:
 if option == "-station":
     station = sys.argv[2]
     dfs = []
-    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_original_sampling_flags.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, 3)
+    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_original_sampling_flags.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, 3,logger)
     dfs.append([df[df.quality != c.badqc][['data']],'Orig. sampling SLEV : OK', '#2980b9',311,1.2,'o','None', 0, 0.05, 0.5])
     dfs.append([df[df.quality == c.badqc][['data']],'Orig. sampling SLEV : BAD','#e71111',311,2.5,'o','None', 0, 0.05, 0.5])
-    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_5min_interpolated.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None)
-    dfs.append([df[['data']],'5-min interpolation','#2980b9',312,1.2,'o','solid', 0, 0.05, 0.5])
-    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_hourly_slev.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None)
-    dfs.append([df[['data']],'Hourly SLEV','#FA8072',312,3.5,'o','solid', 0, 0.05, 0.5])
-    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_hourly_surge.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None)
+    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_5min_interpolated.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None,logger)
+    dfs.append([df[['data']],'5-min interpolation','#2980b9',312,1.2,'o','None', 0, 0.05, 0.5])
+    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_hourly_slev.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None,logger)
+    dfs.append([df[['data']],'Hourly SLEV','#FA8072',312,3.5,'o','None', 0, 0.05, 0.5])
+    df = iofilehandler.txtfile2dataframe(config['general']['outputfolder']+station+'_hourly_surge.out', None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None,logger)
     dfs.append([df[['data']],'Hourly Surge','#229954',313,1.8,'o','None', 0, 2, 0.5])
     visualize.plotsubplots(dfs,stations[station]['name'])
 elif option == "-files":
@@ -33,13 +42,14 @@ elif option == "-files":
     dfs = []
     for file in files:
         try:
-            df = iofilehandler.txtfile2dataframe(file, None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, 3)
+            df = iofilehandler.txtfile2dataframe(file, None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, 3,logger)
             dfs.append([df[df.quality != c.badqc][['data']],file+':OK','#2274a5',1.5,'o','None'])
             dfs.append([df[df.quality == c.badqc][['data']],file+':BAD','#e71111',2.5,'o','None'])
         except IndexError:
-            df = iofilehandler.txtfile2dataframe(file, None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None)
+            df = iofilehandler.txtfile2dataframe(file, None, [0,1], "\"%Y-%m-%d%H:%M:%S\"", 2, None,logger)
             dfs.append([df[['data']],file,'#2274a5',1.5,'o','None'])
     visualize.plot(dfs,sys.argv[2])
 else:
+    logger.error("Unknown option. Please, use -station <stationid> or -files <file1,file2,file3>")
     print("Unknown option. Please, use -station <stationid> or -files <file1,file2,file3>")
 
